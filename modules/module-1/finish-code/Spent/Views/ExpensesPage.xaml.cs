e81c1758f0af43762b61f4b1ec38c0c3132d3dae
Pierce Boggan <pierce.boggan@xamarin.com>
2016-09-19 16:08:57 -0400

    Completed module 1.

---
+++ modules/module-1/finish-code/Spent/Views/ExpensesPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Spent
+{
+	public partial class ExpensesPage : ContentPage
+	{
+		public ExpensesPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new ExpensesViewModel();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			SubscribeToMessages();
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+
+			UnsubscribeFromMessages();
+		}
+
+		void SubscribeToMessages()
+		{
+			MessagingCenter.Subscribe<ExpensesViewModel, Expense>(this, "NavigateToDetail", async (obj, expense) =>
+			{
+				if (expense != null)
+				{
+					await Navigation.PushAsync(new ExpenseDetailPage(expense));
+				}
+			});
+		}
+
+		void UnsubscribeFromMessages()
+		{
+			MessagingCenter.Unsubscribe<ExpensesViewModel, Expense>(this, "NavigateToDetail");
+		}
+	}
+}
