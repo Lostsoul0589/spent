98fca9cecc6afc951fa71464d9cdc0ff1749bfbc
Pierce Boggan <pierce.boggan@xamarin.com>
2016-09-20 00:15:56 -0400

    Progress on module 2 thus far.

---
+++ modules/module-2/starter-code/Spent/Views/ExpensesPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Spent
+{
+	public partial class ExpensesPage : ContentPage
+	{
+		public ExpensesPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new ExpensesViewModel();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			SubscribeToMessages();
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+
+			UnsubscribeFromMessages();
+		}
+
+		void SubscribeToMessages()
+		{
+			MessagingCenter.Subscribe<ExpensesViewModel, Expense>(this, "NavigateToDetail", async (obj, expense) =>
+			{
+				if (expense != null)
+				{
+					await Navigation.PushAsync(new ExpenseDetailPage(expense));
+				}
+			});
+
+			MessagingCenter.Subscribe<ExpensesViewModel, string>(this, "Navigate", async (obj, s) =>
+			{
+				if (s == "NewExpensePage")
+				{
+					await Navigation.PushAsync(new NewExpensePage());
+				}
+			});
+
+			MessagingCenter.Subscribe<ExpensesViewModel, string>(this, "Error", (obj, s) =>
+			{
+				DisplayAlert("Error", s, "OK");
+			});
+		}
+
+		void UnsubscribeFromMessages()
+		{
+			MessagingCenter.Unsubscribe<ExpensesViewModel, Expense>(this, "NavigateToDetail");
+			MessagingCenter.Unsubscribe<ExpensesViewModel, string>(this, "Navigate");
+			MessagingCenter.Unsubscribe<NewExpenseViewModel, string>(this, "Error");
+		}
+	}
+}
